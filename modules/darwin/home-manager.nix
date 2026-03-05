e15cfe14cfff3ccd5f4064893fd86c4c21c3d2d7
kacper <corgodev@gmail.com>
2026-03-05 22:25:36 +0100

    feat: add claude-code to home packages

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -1,8 +1,12 @@
 { username, ... }: {
   users.users.${username}.home = "/Users/${username}";
 
-  home-manager.users.${username} = { ... }: {
+  home-manager.users.${username} = { pkgs, ... }: {
     home.homeDirectory = "/Users/${username}";
+
+    home.packages = with pkgs; [
+      claude-code
+    ];
 
     programs.zsh.shellAliases = {
       rebuild = "sudo darwin-rebuild switch --flake ~/projects/nix-config; source ~/.zshrc";
